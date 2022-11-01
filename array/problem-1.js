fcec7c3ffaa2a99b39218c38d53440c18001eb31
Ikram Akbar <ikramakbar71@gmail.com>
2022-11-01 12:48:21 +0600

    few more method addded - for of, forEach

--- array/problem-1.js
+++ array/problem-1.js
@@ -15,10 +15,25 @@ let sortedArray = demoArray.sort((ele1, ele2) => {
 
 
 
-// sum of the array
+// sum of the array - using traditional array
 let sumOfTheArray = 0;
 
 for (let i = 0; i < demoArray.length; i++) {
     sumOfTheArray += demoArray[i];
 }
 console.log(sumOfTheArray);
+// using for .. of
+
+let sum = 0;
+for (const number of demoArray) {
+    sum += number;
+}
+console.log(sum);
+
+// using Array Method ForEach ()
+
+let result = 0;
+demoArray.forEach((num) => {
+    result += num;
+})
+console.log(result);
